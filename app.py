85569d717bc87f9a82d25186e51f2dc5beee7aa3
Assan Bekkaliyev <47390497+asnbkk@users.noreply.github.com>
2023-06-07 14:59:01 +0600

    reafactored

--- app.py
+++ app.py
@@ -1,51 +1,75 @@
-from flask import Flask, request, jsonify
-
-import torch
-from transformers import AutoTokenizer, AutoModel
-import numpy as np
 import joblib
-
+import numpy as np
+import torch
+from flask import Flask, jsonify, request
+from transformers import AutoModel, AutoTokenizer
 
 app = Flask(__name__)
 
 label_encoder = joblib.load("label_encoder.joblib")
 lr_model = joblib.load("logistic_regression_model.pkl")
 
-tokenizer = AutoTokenizer.from_pretrained("kz-transformers/kaz-roberta-conversational")
-model = AutoModel.from_pretrained("kz-transformers/kaz-roberta-conversational")
+pret_tokenizer = AutoTokenizer.from_pretrained(
+    "kz-transformers/kaz-roberta-conversational"
+)
+pret_model = AutoModel.from_pretrained("kz-transformers/kaz-roberta-conversational")
 
 
-def make_prediction(text):
-    text_embed = embed_bert_cls(text, model, tokenizer)
+def make_prediction(text: str) -> tuple:
+    """
+    Make a prediction on the given text.
+
+    Args:
+        text (str): The input text.
+
+    Returns:
+        tuple: A tuple containing the predicted label and the prediction probabilities.
+    """
+    text_embed = embed_bert_cls(text, pret_model, pret_tokenizer)
     pred = lr_model.predict_proba(np.reshape(text_embed, (1, -1)))
     res = label_encoder.inverse_transform(pred.argmax(axis=1))
     return res, pred[0]
 
 
-def embed_bert_cls(text, model, tokenizer):
-    t = tokenizer(text, padding=True, truncation=True, return_tensors="pt")
+def embed_bert_cls(
+    text: str, model: torch.nn.Module, tokenizer: AutoTokenizer
+) -> np.ndarray:
+    """
+    Embed the text using BERT model.
+
+    Args:
+        text (str): The input text.
+        model: The BERT model.
+        tokenizer: The BERT tokenizer.
+
+    Returns:
+        np.ndarray: The embedded representation of the text.
+    """
+    tokenizer_ = tokenizer(text, padding=True, truncation=True, return_tensors="pt")
     with torch.no_grad():
-        model_output = model(**{k: v.to(model.device) for k, v in t.items()})
+        model_output = model(**{k: v.to(model.device) for k, v in tokenizer_.items()})
     embeddings = model_output.last_hidden_state[:, 0, :]
     embeddings = torch.nn.functional.normalize(embeddings)
     return embeddings[0].cpu().numpy()
 
 
 @app.route("/predict", methods=["POST"])
-def predict():
-    data = request.get_json()  # Get the JSON data from the POST request
-    text = data["text"]  # Extract the input text from the JSON data
+def predict() -> jsonify:
+    """
+    Handle POST request for prediction.
 
-    # Call the make_prediction function
+    Returns:
+        jsonify: The JSON response containing the prediction and probabilities.
+    """
+    data = request.get_json()
+    text = data["text"]
+
     prediction, probabilities = make_prediction(text)
 
-    # Create a response JSON object with the prediction and probabilities
     response = {
         "prediction": prediction[0],
         "probabilities": float(np.max(probabilities)),
     }
-
-    # Return the response as JSON
     return jsonify(response)
 
 
